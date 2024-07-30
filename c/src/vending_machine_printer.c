2872295bdb7d77615223a2d51d975d36c6d99d53
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2024-07-30 19:26:20 +0000

    writeback: Move I_DIRTY_PAGES handling

--- c/src/vending_machine_printer.c
+++ c/src/vending_machine_printer.c
@@ -35,4 +35,20 @@ print_coins(char* buffer, const int* coins, int coins_length) {
     for (int i = 0; i < coins_length; ++i) {
         snprintf(current_coin, 5, "%2d, ", coins[i]);
         if (i == 0) {
-            strncpy(array_c
+            strncpy(array_contents, current_coin, 5);
+        } else {
+            strncat(array_contents, current_coin, 5);
+        }
+    }
+
+    array_contents[max_array_size -1] = '\0';
+
+    sprintf(buffer, "{%s}", array_contents );
+}
+
+int
+buffer_size_for_coins(int coins_length) {
+    // two for {} plus termination plus 4 chars per coin
+    return 3 + (coins_length * 4);
+}
+
