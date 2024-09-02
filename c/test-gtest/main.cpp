9abbb42149af4adc00d0aa509466f7a1d409dd0b
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2024-09-02 12:01:13 +0000

    e2e

--- c/test-gtest/main.cpp
+++ c/test-gtest/main.cpp
@@ -1,2 +1,6 @@
 #define APPROVALS_GOOGLETEST
-#include "ApprovalT
+#include "ApprovalTests.hpp"
+
+#include <memory>
+auto defaultReporterDisposer =
+        ApprovalTests::Approvals::useAsDefaultReporter(std::make_shared<ApprovalTests::ClipboardReporter>());
---
+++ c/test-gtest/vending_machine_gtest.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+using namespace std;
+
+extern "C"
+{
+#include "vending_machine_printer.h"
+#include "vending_machine.h"
+#include "coin.h"
+#include "VendingMachineTest.h"
+}
+
+TEST_F(VendingMachineTest, InsertCoins) {
+
+    ASSERT_EQ("INSERT COIN", string(machine->display));
+
+    insert_coin(machine, coin_with_name("nickel"));
+
+    ASSERT_EQ("$0.05", string(machine->displ
