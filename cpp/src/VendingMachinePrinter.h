98dd4e63882742c74ce527fff8ba0c9d906a7346
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2024-09-28 17:01:18 +0000

    Version 1.6

--- cpp/src/VendingMachinePrinter.h
+++ cpp/src/VendingMachinePrinter.h
@@ -6,4 +6,18 @@
 
 class VendingMachinePrinter {
 public:
-    explicit
+    explicit VendingMachinePrinter(VendingMachine* machine);
+
+    virtual ~VendingMachinePrinter();
+
+    std::string print();
+    std::string formatLine(const std::string& key, const std::string& value) const;
+    std::string printVector(const std::vector<int>* items) const;
+private:
+    VendingMachine* machine;
+
+    int columns;
+};
+
+
+#endif //VENDINGMACHINEAPPROVALKATA_VENDINGMACHINEPRINTER_H
