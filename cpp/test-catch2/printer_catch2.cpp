7f5a2916362029aab2c7d84d8fc5484e6ce658ec
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2024-10-21 13:44:34 +0000

    Readme improvements

--- cpp/test-catch2/printer_catch2.cpp
+++ cpp/test-catch2/printer_catch2.cpp
@@ -7,4 +7,22 @@ using namespace std;
 
 TEST_CASE ("VendingMachinePrinter") {
     auto* machine = new VendingMachine();
-    auto printer = V
+    auto printer = VendingMachinePrinter(machine);
+
+    SECTION("format line") {
+        CHECK(printer.formatLine("foo", "bar") == "foo                                                      bar\n");
+    }
+
+    SECTION("format list of strings") {
+        auto* coins = new std::vector<int>(0, 0);
+        coins->push_back(5);
+        coins->push_back(10);
+        CHECK(printer.printVector(coins) == "{5, 10}");
+    }
+
+    delete machine;
+}
+
+
+
+
