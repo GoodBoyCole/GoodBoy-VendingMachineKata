d1250d4664b746f769ac24782253f185fd71394e
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-01-12 15:58:20 +0000

    blog changes

---
+++ java/src/main/java/org/codingdojo/vendingmachine/VendingMachine.java
@@ -0,0 +1,13 @@
+package org.codingdojo.vendingmachine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class VendingMachine {
+
+    private String display;
+    private List<Integer> coins;
+    private List<Integer> returns;
+    private Integer balance;
+    private Integer[] a
