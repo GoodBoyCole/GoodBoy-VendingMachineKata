ff6efcd74a249fe3e20e0d31e521eac5807fb531
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-01-25 09:41:31 +0000

    intelli_plug: initialize ip_info struct element during driver init

--- java/src/main/java/org/codingdojo/vendingmachine/VendingMachine.java
+++ java/src/main/java/org/codingdojo/vendingmachine/VendingMachine.java
@@ -10,4 +10,20 @@ public class VendingMachine {
     private List<Integer> coins;
     private List<Integer> returns;
     private Integer balance;
-    private Integer[] a
+    private Integer[] acceptedCoins;
+
+    public VendingMachine() {
+        this("", new ArrayList<>(), new ArrayList<>(), 0, new Integer[]{5, 10, 25});
+    }
+
+    public VendingMachine(String display, List<Integer> coins, List<Integer> returns, Integer balance, Integer[] acceptedCoins) {
+        this.display = display;
+        this.coins = coins;
+        this.returns = returns;
+        this.balance = balance;
+        this.acceptedCoins = acceptedCoins;
+
+        displayBalance();
+    }
+
+    private void displayBalance()
