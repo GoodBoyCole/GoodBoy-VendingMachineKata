7806a2c185bea53a524b2df78fa62945f0e9527d
GoodBoyCole <202495863+GoodBoyCole@users.noreply.github.com>
2025-01-29 15:04:29 +0000

    Moving files to Entities.

--- python/vending_machine_printer.py
+++ python/vending_machine_printer.py
@@ -2,4 +2,21 @@ from vending_machine import VendingMachine
 
 
 class VendingMachinePrinter:
-    def __init__(self, machine : VendingMachine)
+    def __init__(self, machine : VendingMachine):
+        self.columns = 60
+        self.machine = machine
+
+    def print(self):
+        text = "VendingMachine\n"
+        # TODO: finish this
+        return text
+
+    def format_line_with_whitespace(self, name, value):
+        """
+        Convenience function that lays out a name and a value at either ends of a fixed-width line.
+        eg if you call it with name="Foo" value="Bar" it will return
+
+        Foo                                       Bar
+
+        """
+        va
